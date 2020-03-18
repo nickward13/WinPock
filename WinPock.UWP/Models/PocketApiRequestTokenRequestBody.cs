f2808c000e07947603e5d99ba1587808146b3462
Nick Ward <nickward13@hotmail.com>
2020-03-18 17:04:26 +1100

    Add project files.

---
+++ WinPock.UWP/Models/PocketApiRequestTokenRequestBody.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WinPock.Models
+{
+    class PocketApiRequestTokenRequestBody
+    {
+        [JsonPropertyName("consumer_key")]
+        public string ConsumerKey { get; set; }
+        [JsonPropertyName("redirect_uri")]
+        public string RedirectUri { get; set; }
+    }
+}
---
+++ WinPock.UnitTest/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WinPock.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int i = 1;
+            Assert.AreEqual(1, i);
+        }
+    }
+}
