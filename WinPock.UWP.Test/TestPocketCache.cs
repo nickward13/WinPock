f8a4706dca91ff492cd0f0a8ed2d269991da4a7e
Nick Ward <nickward13@hotmail.com>
2020-04-02 17:09:10 +1100

    added PocketCache

---
+++ WinPock.UWP.Test/TestPocketCache.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PocketApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinPock.UWP.Test
+{
+    [TestClass]
+    public class TestPocketCache
+    {
+        [TestMethod]
+        public async Task TestInitializationAsync()
+        {
+            PocketClient pocketClient = new PocketClient(Secrets.AccessToken);
+            PocketCache pocketCache = new PocketCache(pocketClient);
+            await TestSyncArticlesAsync(pocketCache);
+            Assert.IsNotNull(pocketCache);
+            Assert.AreNotEqual(0, pocketCache.PocketItems.Count);
+            Assert.IsNotNull(pocketCache.LastSyncDateTime);
+        }
+
+        private static async Task TestSyncArticlesAsync(PocketCache pocketCache)
+        {
+            await pocketCache.SyncArticlesAsync();
+        }
+    }
+}
---
+++ WinPock.UWP/PocketCache.cs
@@ -0,0 +1,32 @@
+﻿using PocketApi;
+using PocketApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinPock.UWP
+{
+    public class PocketCache
+    {
+        private PocketClient _pocketClient;
+        public List<PocketItem> PocketItems { get; set; }
+        public DateTime LastSyncDateTime { get; private set; }
+
+        public PocketCache(PocketClient pocketClient)
+        {
+            _pocketClient = pocketClient;
+            LastSyncDateTime = new DateTime(1970, 1, 1);
+            PocketItems = new List<PocketItem>();
+        }
+
+        public async Task SyncArticlesAsync()
+        {
+            foreach (PocketItem pocketItem in await _pocketClient.GetPocketItemsAsync(LastSyncDateTime))
+                    PocketItems.Add(pocketItem);
+            
+            LastSyncDateTime = DateTime.Now;
+        }
+    }
+}
