958ad4955f81ee6b01c434184d1c4402b46048b8
Nick Ward <nickward13@hotmail.com>
2020-04-06 09:55:09 +1000

    added add and delete PocketItem on cache

--- WinPock.UWP.Test/TestPocketCache.cs
+++ WinPock.UWP.Test/TestPocketCache.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PocketApi;
+using PocketApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,20 +12,61 @@ namespace WinPock.UWP.Test
     [TestClass]
     public class TestPocketCache
     {
-        [TestMethod]
-        public async Task TestInitializationAsync()
+        private PocketClient pocketClient;
+        private string testArticleUrl = "https://www.theage.com.au/culture/music/inside-the-private-world-of-nick-cave-love-life-and-doodles-20200326-p54e4z.html";
+
+        public TestPocketCache()
         {
-            PocketClient pocketClient = new PocketClient(Secrets.AccessToken);
+            pocketClient = new PocketClient(Secrets.AccessToken);
+        }
+
+        [TestMethod]
+        public async Task TestSyncArticlesAsync()
+        {
             PocketCache pocketCache = new PocketCache(pocketClient);
-            await TestSyncArticlesAsync(pocketCache);
+
+            await pocketCache.SyncArticlesAsync();
+
             Assert.IsNotNull(pocketCache);
             Assert.AreNotEqual(0, pocketCache.PocketItems.Count);
             Assert.IsNotNull(pocketCache.LastSyncDateTime);
         }
 
-        private static async Task TestSyncArticlesAsync(PocketCache pocketCache)
+        [TestMethod]
+        public async Task TestAddArticleAsync()
         {
+            PocketCache pocketCache = new PocketCache(pocketClient);
             await pocketCache.SyncArticlesAsync();
+
+            if (pocketCache.PocketItems.Any(pi => pi.GivenUrl == testArticleUrl))
+                await pocketClient.DeletePocketItemAsync(pocketCache.PocketItems.First(pi => pi.GivenUrl == testArticleUrl));
+
+            int articleCountStart = pocketCache.PocketItems.Count;
+            await pocketCache.AddArticleAsync(new Uri(testArticleUrl));
+            int articleCountEnd = pocketCache.PocketItems.Count;
+
+            Assert.AreNotEqual(articleCountStart, articleCountEnd);
+            Assert.IsTrue(articleCountEnd > articleCountStart);
+            Assert.IsTrue(articleCountEnd == articleCountStart + 1);
+        }
+
+        [TestMethod]
+        public async Task TestDeleteArticleAsync()
+        {
+            PocketCache pocketCache = new PocketCache(pocketClient);
+            await pocketCache.SyncArticlesAsync();
+
+            if (!pocketCache.PocketItems.Any(pi => pi.GivenUrl == testArticleUrl))
+                await pocketClient.AddPocketItemAsync(new Uri(testArticleUrl));
+
+            int articleCountStart = pocketCache.PocketItems.Count;
+            PocketItem pocketItem = pocketCache.PocketItems.First(pi => pi.GivenUrl == "https://www.theage.com.au/culture/music/inside-the-private-world-of-nick-cave-love-life-and-doodles-20200326-p54e4z.html");
+            await pocketCache.DeleteArticleAsync(pocketItem);
+            int articleCountEnd = pocketCache.PocketItems.Count;
+
+            Assert.AreNotEqual(articleCountStart, articleCountEnd);
+            Assert.IsTrue(articleCountEnd < articleCountStart);
+            Assert.IsTrue(articleCountEnd == articleCountStart - 1);
         }
     }
 }
--- WinPock.UWP/PocketCache.cs
+++ WinPock.UWP/PocketCache.cs
@@ -2,6 +2,7 @@
 using PocketApi.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,22 +12,54 @@ namespace WinPock.UWP
     public class PocketCache
     {
         private PocketClient _pocketClient;
-        public List<PocketItem> PocketItems { get; set; }
+        public ObservableCollection<PocketItem> PocketItems { get; set; }
         public DateTime LastSyncDateTime { get; private set; }
 
         public PocketCache(PocketClient pocketClient)
         {
             _pocketClient = pocketClient;
             LastSyncDateTime = new DateTime(1970, 1, 1);
-            PocketItems = new List<PocketItem>();
+            PocketItems = new ObservableCollection<PocketItem>();
         }
 
         public async Task SyncArticlesAsync()
         {
-            foreach (PocketItem pocketItem in await _pocketClient.GetPocketItemsAsync(LastSyncDateTime))
-                    PocketItems.Add(pocketItem);
-            
-            LastSyncDateTime = DateTime.Now;
+            DateTime newSyncDateTime = DateTime.UtcNow;
+            IEnumerable<PocketItem> pocketItems = await _pocketClient.GetPocketItemsAsync(LastSyncDateTime);
+
+            foreach (PocketItem pocketItem in pocketItems)
+            {
+                switch(pocketItem.Status)
+                {
+                    case "0":
+                        PocketItems.Add(pocketItem);
+                        break;
+                    case "1":
+                        if (PocketItems.Any(pi => pi.Id == pocketItem.Id))
+                            PocketItems.First(pi => pi.Id == pocketItem.Id).Status = "1";
+                        else
+                            PocketItems.Add(pocketItem);                        
+                        break;
+                    case "2":
+                        PocketItems.Remove(PocketItems.First(pi => pi.Id == pocketItem.Id));
+                        break;
+                }
+            }
+
+            LastSyncDateTime = newSyncDateTime;
         }
+
+        public async Task AddArticleAsync(Uri uri)
+        {
+            PocketItem pocketItem = await _pocketClient.AddPocketItemAsync(uri);
+            await SyncArticlesAsync();
+        }
+
+        public async Task DeleteArticleAsync(PocketItem pocketItem)
+        {
+            await _pocketClient.DeletePocketItemAsync(pocketItem);
+            await SyncArticlesAsync();
+        }
+
     }
 }
