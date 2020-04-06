b83b7301d05be6c89ffba22c60b183693b9dbad4
Nick Ward <nickward13@hotmail.com>
2020-04-06 11:23:21 +1000

    added PocketCacheSaver

---
+++ WinPock.UWP.Test/PocketCacheSaverTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PocketApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinPock.UWP.Test
+{
+    [TestClass]
+    public class PocketCacheSaverTest
+    {
+        [TestMethod]
+        public async Task TestSave()
+        {
+            PocketClient pocketClient = new PocketClient(Secrets.AccessToken);
+            PocketCache pocketCache = new PocketCache(pocketClient);
+            await pocketCache.SyncArticlesAsync();
+
+            PocketCacheSaver pocketCacheSaver = new PocketCacheSaver(Windows.Storage.ApplicationData.Current.LocalFolder);
+            bool result = await pocketCacheSaver.SaveCacheAsync(pocketCache);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task TestLoad()
+        {
+            PocketClient pocketClient = new PocketClient(Secrets.AccessToken);
+            PocketCache pocketCache = new PocketCache(pocketClient);
+
+            PocketCacheSaver pocketCacheSaver = new PocketCacheSaver(Windows.Storage.ApplicationData.Current.LocalFolder);
+            pocketCache = await pocketCacheSaver.LoadCacheAsync(pocketCache);
+
+            Assert.IsNotNull(pocketCache);
+            Assert.AreNotEqual(0, pocketCache.PocketItems.Count);
+            Assert.IsNotNull(pocketCache.LastSyncDateTime);
+        }
+    }
+}
---
+++ WinPock.UWP/PocketCacheSaver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace WinPock.UWP
+{
+    public class PocketCacheSaver
+    {
+        private StorageFolder _storageFolder;
+
+        public PocketCacheSaver(StorageFolder storageFolder)
+        {
+            _storageFolder = storageFolder;
+        }
+        
+        public async Task<bool> SaveCacheAsync(PocketCache pocketCache)
+        {
+            StorageFile storageFile = await _storageFolder.CreateFileAsync("PocketCache.json", CreationCollisionOption.ReplaceExisting);
+            try
+            {
+                string pocketCacheString = JsonSerializer.Serialize(pocketCache);
+                File.WriteAllText(storageFile.Path, pocketCacheString);
+                return true;
+            } catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<PocketCache> LoadCacheAsync(PocketCache pocketCache)
+        {
+            try
+            {
+                StorageFile storageFile = await _storageFolder.GetFileAsync("PocketCache.json");
+                string pocketCacheString = File.ReadAllText(storageFile.Path);
+                pocketCache = JsonSerializer.Deserialize<PocketCache>(pocketCacheString);
+                return pocketCache;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
